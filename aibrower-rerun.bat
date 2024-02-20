d1dfd69d99eafac55cfc044b4844ab8695f5c71a
zhouzihong <1635757679@qq.com>
2024-02-20 15:48:00 +0800

    aibrower

--- aibrower-rerun.bat
+++ aibrower-rerun.bat
@@ -1,15 +1,2 @@
-@echo off
-setlocal enabledelayedexpansion
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":10000 :11000"') do (
-  set "pid=%%a"
-  if not defined !pid! (
-    taskkill /PID !pid! /F
-    set "!pid!=1"
-  )
-)
-
-cd C:\Users\zhouzihong\Desktop\aiarticle\aiarticle
-start /b mvn spring-boot:run >nul 2>nul
-
-cd C:\Users\zhouzihong\Desktop\aibrower\server
-start /b node app.js >nul 2>nul
+cd C:\Users\zhouzihong\Desktop\aibrower\rerun
+node server.js
